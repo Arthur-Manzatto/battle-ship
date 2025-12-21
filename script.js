d6f60c838a99a2c3b6e2607dadee742f82e2f466
Arthur-Manzatto <arthur.manzatto2805@gmail.com>
2025-12-21 15:31:43 -0300

    Finished the Choose GM screen html/css

---
+++ script.js
@@ -0,0 +1,4 @@
+function chooseGM(){ //Function that shows the Game mode options to the player
+    document.getElementById("info-container").style.display = "none";
+    document.getElementById("info-container2").style.display = "flex";
+}
