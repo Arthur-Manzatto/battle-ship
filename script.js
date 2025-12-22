98c668d183e311bf7e9966cbe99a1a52000ed666
Arthur-Manzatto <arthur.manzatto2805@gmail.com>
2025-12-21 22:40:13 -0300

    The player grids are being dynamically generated

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ function showPlayerSide(player) {
     document.getElementById("p2S").style.display = "flex";
   }
 }
+
+
+function start() {
+  showScreen('gameScreen');
+
+  const gridp1 = document.getElementById("grid-p1");
+  const gridp2 = document.getElementById("grid-p2");
+
+  const gridNumber = Number(document.getElementById("gridSize").value);
+  const gridTotalSize = gridNumber * gridNumber;
+
+  gridp1.innerHTML = "";
+  gridp2.innerHTML = "";
+
+  gridp1.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+  gridp2.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+
+  for (let i = 0; i < gridTotalSize; i++) {
+    const water1 = document.createElement("div");
+    water1.classList.add("water");
+
+    const water2 = document.createElement("div");
+    water2.classList.add("water");
+
+    gridp1.appendChild(water1);
+    gridp2.appendChild(water2);
+  }
+}
