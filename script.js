5de4d790d943f27de3506df598b4db892a4f0b61
Arthur-Manzatto <arthur.manzatto2805@gmail.com>
2025-12-22 19:48:24 -0300

    Added a working toast that shows game tips

--- script.js
+++ script.js
@@ -7,19 +7,39 @@ function showScreen(screenId) { //Function that shows the different game screens
   document.getElementById(screenId).classList.add("active"); //At the end, it enables (Adding the class active) the screen that you've sent as parameter :)
 }
 
-function showPlayerSide(player) {
+var currentPlayer = 1;
+function showPlayerSide(player) { //Function that changes the player sied
   if (player == 1) {
     document.getElementById("p1S").style.display = "flex";
     document.getElementById("p2S").style.display = "none";
+    currentPlayer = 1;
   } else {
     document.getElementById("p1S").style.display = "none";
     document.getElementById("p2S").style.display = "flex";
+    currentPlayer = 2;
   }
 }
+
+const toast = document.querySelector(".toast");
+
+function showToast(message, player) { //Show a toast with game tips, so then players know what to do
+  toast.classList.add("active"); //Add the class that pop the toast at the screen
+
+  document.getElementById("text1").innerText = "PLAYER " + player;
+  document.getElementById("text2").innerText = message;
+
+  // Optional auto-hide after 4s
+  setTimeout(() => {
+    toast.classList.remove("active");
+  }, 10000);
+}
+
 
 
 function start() {
   showScreen('gameScreen');
+  showPlayerSide(1);
+  showToast("PLACE YOUR BOATS", 1);
 
   const gridp1 = document.getElementById("grid-p1");
   const gridp2 = document.getElementById("grid-p2");
@@ -30,17 +50,21 @@ function start() {
   gridp1.innerHTML = "";
   gridp2.innerHTML = "";
 
-  gridp1.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+  gridp1.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`; // Here I set the size (Y x Y) of the grid
   gridp2.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
 
-  for (let i = 0; i < gridTotalSize; i++) {
+  for (let i = 0; i < gridTotalSize; i++) { //This loop creates the "water" blocks
     const water1 = document.createElement("div");
     water1.classList.add("water");
 
     const water2 = document.createElement("div");
     water2.classList.add("water");
 
-    gridp1.appendChild(water1);
+    gridp1.appendChild(water1); //Here I'm adding the water div to the grid element 
     gridp2.appendChild(water2);
   }
+}
+
+function placingBoats() {
+
 }
