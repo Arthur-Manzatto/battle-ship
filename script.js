c160cc8203d305be57646f4f3b0829696c11f5c4
Arthur-Manzatto <arthur.manzatto2805@gmail.com>
2025-12-21 22:10:58 -0300

    Added the player sides and the showPlayerSide() function

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function showScreen(screenId) { //Function that shows the different game screens
-  
+
   document.querySelectorAll(".screen").forEach(screen => { //Here it selects ALL the sections and remove the class ACTIVE(display flex)
     screen.classList.remove("active");
   });
 
   document.getElementById(screenId).classList.add("active"); //At the end, it enables (Adding the class active) the screen that you've sent as parameter :)
+}
+
+function showPlayerSide(player) {
+  if (player == 1) {
+    document.getElementById("p1S").style.display = "flex";
+    document.getElementById("p2S").style.display = "none";
+  } else {
+    document.getElementById("p1S").style.display = "none";
+    document.getElementById("p2S").style.display = "flex";
+  }
 }
